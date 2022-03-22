0f19b65d26998a0f7d693b25aaa77e013584cade
jdominiclobo <jdominiclobo@gmail.com>
2022-03-22 16:39:23 +0530

    learning next js

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+        <meta name="keywords" content="web development, programming" />
+      </Head>
+      <h2>About</h2>
+    </div>
+  );
+};
+export default About;
